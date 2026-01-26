ccd738b9f3c29733841ecf64dd1149cf0b9762e8
DARK <marksamrat56@gmail.com>
2026-01-26 20:58:22 +0300

    Update __init__.py

--- alf_pkg/__init__.py
+++ alf_pkg/__init__.py
@@ -4,11 +4,8 @@ import importlib
 # ফোল্ডারের ভেতর থাকা মডিউলটি খুঁজে বের করা
 package_dir = os.path.dirname(__file__)
 for file in os.listdir(package_dir):
-    # এখানে 'alcm' এর জায়গায় 'alf' করে দেওয়া হয়েছে
-    if file.startswith("alf") and (file.endswith(".so") or file.endswith(".py")):
+    if file.startswith('alf') and (file.endswith('.so') or file.endswith('.py')):
         module_name = file.split('.')[0]
         # ডাইনামিকভাবে মডিউলটি লোড করা
         globals().update(importlib.import_module(f".{module_name}", __package__).__dict__)
         break
-
-
